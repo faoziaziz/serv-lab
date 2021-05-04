728fd029bf2a1ed0054cfd443c09891995118b0f
Aziz Faozi <admin@labseni.com>
2021-05-04 21:25:23 +0700

    da

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from typing import Optional
-
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
+
 
 
 
@@ -13,6 +13,7 @@ class Item(BaseModel):
     price: float
     tax: Optional[float]=None
     
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
@@ -21,9 +22,11 @@ def read_root():
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
         
+
 @app.get("/justtest")
 def just_test():
     return {"item": "tested"}
+
 @app.post("/items/")
 async def create_item(item: Item):
     return item
